d5c38615645842c32b73355b60ff4b11c0be4034
Piyush8456 <p8use9@gmail.com>
2024-02-19 22:59:57 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters01.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HandleError]
+        public ActionResult Index()
+        {
+            throw new Exception();
+            return View();
+        }
+
+        public ActionResult ErrorPage()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Filters01.MvcApplication" Language="C#" %>
