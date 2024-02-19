a398a945ba5ea1752369b33ae18e47491cf078cd
Piyush8456 <p8use9@gmail.com>
2024-02-20 01:07:44 +0530

    second commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Filters01.filter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,24 +9,31 @@ namespace Filters01.Controllers
 {
     public class HomeController : Controller
     {
-        [HandleError]
+        //[HandleError]
+
         public ActionResult Index()
         {
-            throw new Exception();
+            //throw new Exception();
             return View();
         }
+
+
 
         public ActionResult ErrorPage()
         {
             return View();
+
         }
 
 
-
+        [OutputCache(Duration =10)]
         public ActionResult About()
         {
+              ViewBag.Time = DateTime.Now.ToLongTimeString();
             return View();
         }
+
+        [ManualFilter]
         public ActionResult Contact()
         {
             return View();
---
+++ filter/ManualFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters01.filter
+{
+    public class ManualFilter :FilterAttribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Debug.WriteLine("this is OnActionExecuted");
+
+         }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Debug.WriteLine("This is OnActionExecuting");
+        }
+    }
+}
